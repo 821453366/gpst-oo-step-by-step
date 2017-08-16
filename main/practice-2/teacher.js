2d8217d8f032a47c10cddd135d37d2eb81e37374
821453366 <821453366@qq.com>
2017-08-16 20:21:06 +0800

    practice-2 finish

--- main/practice-2/teacher.js
+++ main/practice-2/teacher.js
@@ -1,9 +1,16 @@
 const Person = require('./person');
 
-module.exports = class Teacher extends Person{
-    constructor(name,age,clazzes){
-        super(name,age);
+module.exports = class Teacher extends Person {
+    constructor(name, age, clazzes) {
+        super(name, age);
         this.clazzes = clazzes;
     }
 
+    introduce() {
+        if (this.clazzes.length === 0) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + this.clazzes[0].number + "," + this.clazzes[1].number + ".";
+        }
+    }
 }
