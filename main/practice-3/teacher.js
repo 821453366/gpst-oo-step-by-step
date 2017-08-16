7eb08991233958d7803ac27542c70c067160bc3c
821453366 <821453366@qq.com>
2017-08-16 22:02:35 +0800

    3 teacher 完成测试10和测试11

--- main/practice-3/teacher.js
+++ main/practice-3/teacher.js
@@ -1,3 +1,34 @@
 const Person = require('./person');
 
-module.exports = class Teacher {}
+module.exports = class Teacher extends Person {
+    constructor(name, age, clazzes) {
+        super(name, age);
+        this.clazzes = clazzes;
+    }
+
+    introduce() {
+
+        if (this.clazzes.length === 0) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + ` I am a Teacher. I teach Class ${this.clazzes[0].number},${this.clazzes[1].number}.`;
+        }
+
+    }
+
+    isTeaching(student) {
+        this.student = student;
+        let count =0;
+
+        this.clazzes.forEach(item => {
+            if(item.student !== undefined){
+                count++;
+            }
+        });
+        if(count > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
