6f92949c50729a89fa6560e12ca5f5f903106417
821453366 <821453366@qq.com>
2017-08-16 12:26:06 +0800

    完成测试student3.和4.

--- main/practice-1/student.js
+++ main/practice-1/student.js
@@ -1,3 +1,15 @@
 const Person = require('./person');
 
-module.exports = class Student {}
+module.exports = class Student {
+    constructor(name, age, klass) {
+        let person = new Person(name, age);
+
+        this.name = person.name;
+        this.age = person.age;
+        this.klass = klass;
+    }
+
+    introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.klass+".";
+    }
+}
