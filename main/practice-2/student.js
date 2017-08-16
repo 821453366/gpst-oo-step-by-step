ebc8b1d73f5879ee25620bcc22f4892fb3daefe2
821453366 <821453366@qq.com>
2017-08-16 19:21:12 +0800

    完成2中teacher第一个测试

--- main/practice-2/student.js
+++ main/practice-2/student.js
@@ -7,7 +7,7 @@ module.exports = class Student extends Person{
     }
     introduce() {
         if (this.id === this.clazz.leader) {
-            return super.introduce() + " I am a Student. I am Leader at Class " + this.clazz.number + ".";
+            return super.introduce() + " I am a Student. I am Leader of Class " + this.clazz.number + ".";
         }
         else {
             return super.introduce() + " I am a Student. I am at Class " + this.clazz.number + ".";
--- main/practice-2/teacher.js
+++ main/practice-2/teacher.js
@@ -1,3 +1,9 @@
 const Person = require('./person');
 
-module.exports = class Teacher {}
+module.exports = class Teacher extends Person{
+    constructor(name,age,clazzes){
+        super(name,age);
+        this.clazzes = clazzes;
+    }
+
+}
