71524610c8b4cea623c54974cda02185d0263a87
821453366 <821453366@qq.com>
2017-08-16 21:19:34 +0800

    完成3中的student测试

--- main/practice-3/student.js
+++ main/practice-3/student.js
@@ -1,3 +1,20 @@
 const Person = require('./person');
 
-module.exports = class Student {}
+module.exports = class Student extends Person {
+    constructor(name, age, clazz) {
+        super(name, age);
+        this.clazz = clazz;
+    }
+
+    introduce() {
+        let result = `${super.introduce()} I am a Student.`;
+
+        if (this.clazz.student === undefined) {
+            return result.concat(` I haven't been allowed to join Class.`)
+        } else if (this.clazz.leader === undefined) {
+            return result.concat(` I am at Class ${this.clazz.number}.`);
+        } else {
+            return result.concat(` I am Leader of Class ${this.clazz.number}.`)
+        }
+    }
+}
