dcb1f0ae1b149d2fea917ee964579a8e2bfa4872
821453366 <821453366@qq.com>
2017-08-16 12:45:00 +0800

    student测试4和测试5

--- main/practice-1/student.js
+++ main/practice-1/student.js
@@ -1,15 +1,12 @@
 const Person = require('./person');
 
-module.exports = class Student {
+module.exports = class Student extends Person{
     constructor(name, age, klass) {
-        let person = new Person(name, age);
-
-        this.name = person.name;
-        this.age = person.age;
+        super(name,age);
         this.klass = klass;
     }
 
     introduce(){
-        return "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.klass+".";
+        return super.introduce()+" I am a Student. I am at Class "+this.klass+".";
     }
 }
